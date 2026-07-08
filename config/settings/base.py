046978ef481dc901361cbd158dfc631894f84d24
Fatih Soyaltun <115159389+fatihsoyaltun@users.noreply.github.com>
2026-07-08 15:57:07 +0300

    fix: use two factor login url

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,6 +113,10 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+LOGIN_URL = '/account/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/account/login/'
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
